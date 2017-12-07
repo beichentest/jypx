e8ba32b60e91a521ad251cb1ff87e4b6d5f27452
lichen <beichen35@sina.com>
2017-12-07 09:13:11 +0800

    test

---
+++ jypx/jypx_model/src/main/java/com/geekcattle/mapper/kjk/KjkPlayTypeMapper.java
@@ -0,0 +1,7 @@
+package com.geekcattle.mapper.kjk;
+
+import com.geekcattle.model.kjk.KjkPlayType;
+import com.geekcattle.util.CustomerMapper;
+
+public interface KjkPlayTypeMapper extends CustomerMapper<KjkPlayType> {
+}

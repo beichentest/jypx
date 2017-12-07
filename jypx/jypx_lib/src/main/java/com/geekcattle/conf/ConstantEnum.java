48dfa4abb4fcca6e0db072a8d9e5fc34bd9860ec
lichen <beichen35@sina.com>
2017-12-07 10:54:23 +0800

    常量修改

--- jypx/jypx_lib/src/main/java/com/geekcattle/conf/ConstantEnum.java
+++ jypx/jypx_lib/src/main/java/com/geekcattle/conf/ConstantEnum.java
@@ -1,5 +1,9 @@
 package com.geekcattle.conf;
-
+/**
+ * 
+ * @author Administrator
+ * 字符常量类 （表名_字段_值含义）
+ */
 public enum ConstantEnum {
 	/**
 	 *  INFO删除状态
@@ -8,7 +12,35 @@ public enum ConstantEnum {
 	/**
 	 * INFO删除状态
 	 */
-	INFO_ENABLE("0");	
+	INFO_ENABLE("0"),
+	/**
+	 *  付费标志（0 未付费）
+	 */
+	KJK_COURSEWARE_PLY_FLAG_NOT("0"),
+	/**
+	 * 付费标志（1 付费）
+	 */
+	KJK_COURSEWARE_PLY_FLAG_IS("1"),
+	/**
+	 * 课间来源 cme
+	 */
+	KJK_COURSEWARE_SOURCE_CME("cme"),
+	/**
+	 * 课间来源 基层
+	 */
+	KJK_COURSEWARE_SOURCE_BASE("基层"),
+	/**
+	 * 课间来源 住院医
+	 */
+	KJK_COURSEWARE_SOURCE_RCT("住院医"),
+	/**
+	 * 课间来源 医护考
+	 */
+	KJK_COURSEWARE_SOURCE_EXAM("医护考"),
+	/**
+	 * 课间来源 乡医
+	 */
+	KJK_COURSEWARE_SOURCE_COUNTRY_DOCTORS("乡医");
 	private String value;
 	private ConstantEnum(String value) {
 		this.value = value;
